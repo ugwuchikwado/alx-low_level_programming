7c54b92b13cb38591858460cded94f713ddcdcac
= <ugwuchikwadoaugustine@gmail.com>
2023-02-21 15:04:10 +0100

    Write a function that prints the alphabet, in lowercase, followed by a new line.

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.c"
+
+/**
+ * print_alphabet - prints out the alphabet
+ * main - uses built in function
+ * Return: 0 if succesful
+ */
+
+void print_alphabet(void)
+{
+	char n;
+	for(n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+
+	_putchar('\n');
+}
