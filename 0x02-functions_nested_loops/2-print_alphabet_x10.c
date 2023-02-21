8a1fb6aecaeeef0e1deaaf6ade2bfab19d5053ae
Chikwado <ugwuchikwadoaugustine@gmail.com>
2023-02-21 15:28:54 +0100

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,6 +10,7 @@ void print_alphabet_x10(void)
 {
 	char n;
 	int i;
+	i = 0;
 
 	while (i < 10)
 	{
@@ -17,8 +18,7 @@ void print_alphabet_x10(void)
 		{
 			_putchar(n);
 		}
-
-		_putchar('\n');
 		i++;
+		_putchar('\n');
 	}
 }
