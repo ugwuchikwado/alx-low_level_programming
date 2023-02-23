b94cd60b06610150727e03f2adab2dea2f0ed4b1
= <ugwuchikwadoaugustine@gmail.com>
2023-02-23 10:59:42 +0100

    print numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_numbers: print numbers from 0 to 9
+ * _putchar: used only twice
+ * Return: always zero
+ */
+
+int _print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
