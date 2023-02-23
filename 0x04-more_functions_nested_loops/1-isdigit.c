71286315aa4f0914c02ff8c1ce45afae1176631d
= <ugwuchikwadoaugustine@gmail.com>
2023-02-23 10:46:51 +0100

    check for digit from 0 through 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for a digit from 0 through 9
+ * @c - digit that is been checked
+ * return 1 un success otherwise return 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
