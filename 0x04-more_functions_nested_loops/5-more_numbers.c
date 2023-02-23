3cd4f5bda2df855ab655edd975fd64e469c15f47
= <ugwuchikwadoaugustine@gmail.com>
2023-02-23 11:35:33 +0100

    print numbers from 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print numbers from 0 to 14 ten times
+ * Return: always 0
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+	
+	i = 0;
+	j = 0;
+	while (i < 10)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
