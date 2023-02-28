c8614f73eac0797ff30f537ecdef6de50e6a492f
= <ugwuchikwadoaugustine@gmail.com>
2023-02-28 12:32:58 +0100

    print the reverse of  string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - print a string in reverse followed by a new line
+ * @s: string to be reversed
+ */
+
+void rev_string(char *s)
+{
+	int len, count, end;
+	char rev;
+
+	len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	len--;
+	for (count = 0; count <= len / 2; count++)
+	{
+		rev = s[count];
+		end = s[len - count];
+		s[count] = end;
+		s[len - count] = rev;
+	}
+}
