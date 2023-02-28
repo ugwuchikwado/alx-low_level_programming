c6c7330311e907a0be3b1c86ddd2b8276a8e59a3
= <ugwuchikwadoaugustine@gmail.com>
2023-02-28 11:55:32 +0100

    lenght of a string

---
+++ 0x05-pointers_arrays_strings/strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: the length of a string
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+}
