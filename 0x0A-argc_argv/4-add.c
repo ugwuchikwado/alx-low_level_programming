e7317dbe6feda3b41c0e430bf4a5fd63cef89c13
Chikwado <ugwuchikwadoaugustine@gmail.com>
2023-03-11 11:39:51 +0100

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,40 +1,54 @@
+#include <limits.h>
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
 
 /**
- * main - function that prints the sum of all arguments it receives
+ * chartoint - function that checks array of characters for integers
+ * @c: array of characters
+ * Return: 0 if successful, -1 if fails
+ */
+int chartoint(char *c)
+{
+	int num;
+	char *ptr;
+
+	num = strtol(c, &ptr, 10);
+	if (ptr[0])
+		return (-1);
+	return (num);
+}
+
+/**
+ * main - function that multiplies two numbers
  * @argc: the size of the argv array, the number of command line arguments
  * @argv: an array containing the program command line arguments
  * Return: 0 if successful
  */
-
 int main(int argc, char *argv[])
 {
-	int nums, i;
-	nums = 0;
+	int i, sum, check;
 
 	if (argc == 1)
 	{
-		printf("%d\n",0);
+		printf("0\n");
+		return (0);
 	}
-	else
+	for (i = 1, sum = 0; i < argc; i++)
 	{
-		for (i = 0; i < argc; i++)
+		check = chartoint(argv[i]);
+		if (check > -1)
 		{
-			if (!isdigit(argv[i]))
-			{
-	
-				printf("Error\n");
-				return (1);
-			}
+			if (INT_MAX - sum > check)
+				sum += check;
 			else
-			{
-				nums = nums + atoi( argv[i]);
-			
-			}
+				check = -1;
 		}
-		printf("%d\n", nums);
+		if (check < 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
 	}
+	printf("%d\n", sum);
 	return (0);
-	}
+}
