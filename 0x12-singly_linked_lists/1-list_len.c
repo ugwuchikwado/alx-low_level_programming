3ebbe4fb16f8425e6c9101d50cdf4332e2282f51
= <ugwuchikwadoaugustine@gmail.com>
2023-10-19 11:44:47 +0100

    linked list task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - prints the lenght of a list
+ *
+ * @h : linked list
+ *
+ * Return : the lenght of a list
+ *
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while(h != null)
+	{
+		h = h.next;
+		count++;
+	}
+
+	return (count);
+}
