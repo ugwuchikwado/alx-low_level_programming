9ac2bf1fbbbeaa7f0fac45388dc7dc88562805bd
Chikwado <ugwuchikwadoaugustine@gmail.com>
2023-10-19 12:10:16 +0100

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,8 +15,7 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
-
+	
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -25,8 +24,6 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
-		count++;
 	}
 
-	return (count);
 }
