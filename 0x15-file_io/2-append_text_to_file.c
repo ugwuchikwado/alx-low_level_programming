063603f91ea025cbb3e94671a69949f674ee48a7
= <ugwuchikwadoaugustine@gmail.com>
2023-10-31 08:15:40 +0100

    file IO task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * append_text_to_file - function that appends text at the end
+  * @filename: name of the file to append
+  * @text_content: content to append
+  *
+  * Return: ...
+  */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		if (write(fd, text_content, _strlen(text_content)) == -1)
+			return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c])
+		c++;
+
+	return (c);
+}
+
